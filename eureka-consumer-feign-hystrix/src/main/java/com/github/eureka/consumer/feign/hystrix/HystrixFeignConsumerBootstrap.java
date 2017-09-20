96c01f34b4eb693562cc3dc55d8357dea1f0a9f2
WBingHE <hwb9588@126.com>
2017-09-20 19:11:23 +0800

    Initial commit

--- eureka-consumer-feign-hystrix/src/main/java/com/github/eureka/consumer/feign/hystrix/HystrixFeignConsumerBootstrap.java
+++ eureka-consumer-feign-hystrix/src/main/java/com/github/eureka/consumer/feign/hystrix/HystrixFeignConsumerBootstrap.java
@@ -2,9 +2,11 @@ package com.github.eureka.consumer.feign.hystrix;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
+@EnableCircuitBreaker
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
